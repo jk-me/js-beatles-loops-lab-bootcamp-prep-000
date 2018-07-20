d5de2e6bc9fc31f79bbe5b2c48879b4b850ee11b
Jenny K <jkam17@gmail.com>
2018-07-20 03:58:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-function theBeatlesPlay(am,ai){
-  var n = []
-  for (var i = 0; i< am.length; i++){
-    n.push(`${am[i]} plays ${ai[i]}`)
+function theBeatlesPlay(am,ai){   //array of musicians, instruments
+  var n = []                      //empty array n
+  for (var i = 0; i< am.length; i++){     //loop through musicians Array
+    n.push(`${am[i]} plays ${ai[i]}`)     //appends 'musician plays instrument' to array n
   }
   return n
 }
 
-function johnLennonFacts(af){
-  var nf=[]
-  while (af.length>0){
-    nf.push(`${af.shift()}!!!`)
+function johnLennonFacts(af){       //array of facts
+  var nf=[]                         //empty array nf
+  while (af.length>0){              //while fact array is not empty
+    nf.push(`${af.shift()}!!!`)     //removes item from fact array and appends to array nf
   }
   return nf
 }
 
-function iLoveTheBeatles(n){
-  var x = []
-  do{x.push('I love the Beatles!')
-    n++
+function iLoveTheBeatles(n){        //n is a number
+  var x = []                        //empty array x
+  do{x.push('I love the Beatles!')  //appends to empty array
+    n++                             //while adding to n
   }
-  while (n<15)
+  while (n<15)            //adds 15-n times, once if n>=15
   return x
 }
